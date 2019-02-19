33f4720145753d2a1ead7ea52e3407e956e420f8
Luisa Scavo <luisa@Luisas-MacBook-Pro.local>
2019-02-18 20:49:59 -0500

    grab additional img tag attributes, deconstruct args

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const ImageList = (props) => {
-    const images = props.images.map( image => {
-        return <img src={image.urls.regular} />
-    })
+    const images = props.images.map(({description, id, urls}) => {
+        return <img key={id} alt={description} src={urls.regular} />
+    }) // key goes on root element
     return <div>{images}</div>
 }
 
