d73dd2a6cd2f360dc76c7329768e4a84219769ba
Luisa Scavo <luisascavo@gmail.com>
2019-02-19 10:10:14 -0500

    wire up imageRef

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.imageRef = React.createRef();
+  }
   render() {
     const { description, urls } = this.props.image
     return (
       <div>
         <img 
+          ref={this.imageRef}
           alt={description} 
           src={urls.regular}
         />
