63e7d7354c764073b606f2f6bdc181d765cb4297
Luisa Scavo <luisascavo@gmail.com>
2019-02-19 10:27:01 -0500

    set number of rows for ImageCard and update display

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -4,17 +4,25 @@ class ImageCard extends React.Component {
   constructor(props){
     super(props);
 
+    this.state = { spans: 0 };
+
     this.imageRef = React.createRef();
   }
 
   componentDidMount() {
-    console.log(this.imageRef.current.clientHeight) // This will be zero! We have refs, but data hasn't loaded by the time this is called
+    this.imageRef.current.addEventListener('load', this.setSpans)
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight
+    const spans = Math.ceil(height / 10 + 1) // +1 to ensure round up
+    this.setState({ spans }); // key and value are identical
   }
 
   render() {
     const { description, urls } = this.props.image
     return (
-      <div>
+      <div style={{gridRowEnd: `span ${this.state.spans}`}}>
         <img 
           ref={this.imageRef}
           alt={description} 
